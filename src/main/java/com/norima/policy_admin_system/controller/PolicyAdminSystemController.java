4ca84125aded78350fb32f2942c269b28a48c8ee
Lmarl Saria <lmarlsaria21@gmail.com>
2022-12-07 12:09:37 +0800

    update

--- src/main/java/com/norima/policy_admin_system/controller/PolicyAdminSystemController.java
+++ src/main/java/com/norima/policy_admin_system/controller/PolicyAdminSystemController.java
@@ -4,13 +4,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
